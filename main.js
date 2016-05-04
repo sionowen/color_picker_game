ab8129f438963004177abfb3e8d17c804ce47a11
sionowen <sionowen34@gmail.com>
2016-05-03 20:21:18 -0500

    minimum standard for task met

--- main.js
+++ main.js
@@ -2,15 +2,35 @@ $(document).on('ready', function() {
   var colorArray = ['red', 'peach', 'pink', 'gold'];
 
 
-  $('.colorContainer').append('<div class="red box"></div>')
-  $('.colorContainer').append('<div class="peach box"></div>')
-  $('.colorContainer').append('<div class="pink box"></div>')
-  $('.colorContainer').append('<div class="gold box"></div>')
+  $('.colorContainer').append('<div class="red box"></div>');
+  $('.colorContainer').append('<div class="peach box"></div>');
+  $('.colorContainer').append('<div class="pink box"></div>');
+  $('.colorContainer').append('<div class="gold box"></div>');
 
 
+  $('.red').data('red', 0);
+  $('.peach').data('peach', 1);
+  $('pink').data('pink', 2);
+  $('gold').data('gold', 3);
+  assignWinner();
+  $('.colorContainer').on('click', '.box', function(){
+    if($(this).hasClass('correct')){
+      alert('You got it Right!!!!!!!!!!!!!!!!!!!!');
+      removeWinner();
+      assignWinner();
+    }else {
+      alert('nope');
+    }
 
-  console.log(randomNumber(0, 3));
+  })
 
+  function assignWinner(){
+    var winningIndex = randomNumber(0, 3);
+    $('.' + colorArray[winningIndex]).addClass('correct');
+  }
+  function removeWinner(){
+    $('.correct').removeClass('correct');
+  }
   function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
